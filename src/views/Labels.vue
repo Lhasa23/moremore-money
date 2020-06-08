8221c07ba6257a64782d4325150c5fd10d8e9a20
eleven-shi <sydryy@gmail.com>
2020-06-08 23:02:12 +0800

    标签页面样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,15 +1,58 @@
 <template>
 	<Layout>
-		Labels.vue
+		<ol class="labels">
+			<li v-for="(item, index) in labels" :key="index">
+				<span>{{item}}</span>
+				<icons class="icons" link="right"/>
+			</li>
+		</ol>
+		<div class="add-label-wrapper">
+			<button class="add-label">新增标签</button>
+		</div>
 	</Layout>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+	import {Vue, Component} from 'vue-property-decorator';
+	import {model} from '@/utils/model';
 
-export default Vue.extend({})
+	@Component
+	export default class Labels extends Vue {
+		labels: string[] = model.fetchLabelList();
+	}
 </script>
 
 <style lang="scss" scoped>
+	.labels {
+		background-color: white;
+		padding-left: 16px;
 
+		li {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			font-size: 16px;
+			min-height: 44px;
+			border-bottom: 1px solid #e6e6e6;
+
+			.icons {
+				width: 24px;
+				height: 24px;
+			}
+		}
+	}
+
+	.add-label {
+		height: 40px;
+		padding: 0 10px;
+		border: none;
+		background-color: #B3FF56;
+		border-radius: 5px;
+
+		&-wrapper {
+			text-align: center;
+			padding: 10px 0;
+			margin-top: 24px;
+		}
+	}
 </style>
